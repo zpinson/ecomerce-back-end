4a33c1777e036e556b9b2c534e952e077a13b402
S Pinson <77454522+zpinson@users.noreply.github.com>
2021-03-14 18:17:17 -0400

    update by id function  done for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -43,12 +43,29 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,3 +1,4 @@
+
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
